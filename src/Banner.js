5db7ae6db5cb8ba6d51098cb4fbe2a416f8916c8
kryon22 <shubhamkharabe02@gmail.com>
2022-01-04 11:33:54 +0530

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,29 @@
+import React,{useState} from 'react';
+import './banner.css';
+import {Button} from "@material-ui/core"
+import Search from './Search';
+
+function Banner() {
+
+    const [showSearch, setShowSearch] =useState(false);
+
+    return (
+       
+        
+        <div className='banner'>
+         <div className="banner_search">
+         {showSearch && <Search/>}
+            <Button onClick={()=> setShowSearch(!showSearch)}  className='banner_searchbutton' variant='outlined'>{showSearch ? "Hide" : "Search Dates"}</Button>
+            </div>
+         
+    <div className="banner_info">
+            <h1>Get out of stretch your imagination</h1>
+            <h5>Plan a diffrent kind of getway to uncover  the hidden gems near you</h5>
+            <Button variant='outlined '> Explore Nearby</Button>
+
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Header.css';
+import  { ExpandMore, Language, Search } from '@material-ui/icons';
+import { Avatar } from '@material-ui/core';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img src="https://blogravenridgecottages.files.wordpress.com/2021/04/airbnb-logo.jpeg" className='header_icon' alt="" />
+
+            <div className="header_center">
+                <input type="text" name="" id="" />
+                <Search/>
+            
+            </div>
+
+            <div className="header_right">
+                <p>Become a Host</p>
+                <Language/>
+                <ExpandMore/>
+                <Avatar/>
+            </div>
+        </div>
+
+    )
+}
+
+export default Header;
